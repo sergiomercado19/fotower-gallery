c0a886f3360bcbbe72296a5725870d762a2af4ef
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-10-06 10:13:15 +1100

    Convert *pictures* functions to use the serverless framework (#2)

---
+++ services/apis/pictures/remove_picture.py
@@ -0,0 +1,10 @@
+import json
+
+
+def lambda_handler(event, context):
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    return {
+        "message": "Picture with ID '{}' was removed".format(event.get('pictureId')),
+        "event": event
+    }
