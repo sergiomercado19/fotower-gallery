13b3ddabd0598cc61f6cb4bd64876a48e5c97c43
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-10-12 10:54:40 +1100

    Add functionality to update picture (#3)

--- services/apis/pictures/modify_picture.py
+++ services/apis/pictures/modify_picture.py
@@ -1,19 +1,58 @@
 import json
+import boto3
+import logging
+from botocore.exceptions import ClientError
 
+
+# Set up our logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+# Connect to DynamoDB
+dynamodb = boto3.resource('dynamodb')
+pictures_table = dynamodb.Table('fg-pictures-table')
 
 def lambda_handler(event, context):
+    # Request parsing
+    payload = json.loads(event['body'])
+
     # Request processing
-    pictureId = event['pathParameters']['id']
-    description = event['body']['description']
-    location = event['body']['location']
+    picId = event['pathParameters']['id']
+    description = payload['description']
+    location = payload['location']
 
     # Response formatting
-    body = {
-        "message": "Picture with ID '{}' was modified".format(pictureId)
-    }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    status_code = 200
+    body = {}
 
-    return response
+    # Update db item
+    try:
+        logger.info('Updating specified picture')
+        response = pictures_table.update_item(
+            Key={
+                'picId': picId
+            },
+            UpdateExpression="set description=:d, location=:l",
+            ExpressionAttributeValues={
+                ':d': description,
+                ':l': location
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            logger.warn(response['Error']['Message'])
+            status_code = response['ResponseMetadata']['HTTPStatusCode']
+            body['errors'] = [ response['Error']['Message'] ]
+        else:
+            logger.info('Updated picture with ID [{}]'.format(picId))
+
+    except ClientError as e:
+        logger.warn(e.response['Error']['Message'])
+        status_code = e.response['ResponseMetadata']['HTTPStatusCode']
+        body['errors'] = [ e.response['Error']['Message'] ]
+
+    return {
+        'statusCode': status_code,
+        'body': json.dumps(body)
+    }
