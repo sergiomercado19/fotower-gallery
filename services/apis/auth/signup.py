f04809bdd6c1bb955d8a880c57275896cfd3cfcd
Sergio Mercado Ruiz <sergio.mercadoruiz9@gmail.com>
2020-10-19 22:48:17 +1100

    Add signup API endpoint (#5)
    The hardest part was figuring out how to disable verification so that the user can login using their credentials straight after creating their account.
    This was overcome by having two parts to the request, first creating the user and then, manually, settings its password.

--- services/apis/auth/signup.py
+++ services/apis/auth/signup.py
@@ -20,7 +20,6 @@ def lambda_handler(event, context):
 
    # Package data
     new_user = {
-        'username': payload['username'],
         'email': payload['email'],
         'name': payload['name'],
         'bio': payload['bio'],
@@ -30,10 +29,63 @@ def lambda_handler(event, context):
     # Response formatting
     status_code = 201
     body = {}
-    
-    # Create db item
+
     try:
-        logger.info('Creating user ({})'.format(new_user['username']))
+        # Create Cognito entry for this new user
+        logger.info('Registering user ({})'.format(new_user['email']))
+        response = cognito.admin_create_user(
+            UserPoolId='ap-southeast-2_JP64Xpy5m',
+            Username=new_user['email'],
+            UserAttributes=[
+                {
+                    'Name': 'email_verified',
+                    'Value': 'True'
+                },
+                {
+                    'Name': 'email',
+                    'Value': new_user['email']
+                }
+            ],
+            TemporaryPassword='Password123',
+            DesiredDeliveryMediums=[
+                'EMAIL'
+            ]
+        )
+        
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            logger.warn(response['Error']['Message'])
+            body['errors'] = [ response['Error']['Message'] ]
+            return {
+                'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+                'body': json.dumps(body)
+            }
+
+        else:
+            logger.info('Registered user ({})'.format(new_user['email']))
+
+        # Set password for new user
+        logger.info('Setting user password ({})'.format(new_user['email']))
+        response = cognito.admin_set_user_password(
+            UserPoolId='ap-southeast-2_JP64Xpy5m',
+            Username=new_user['email'],
+            Password=payload['password'],
+            Permanent=True
+        )
+        
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            logger.warn(response['Error']['Message'])
+            body['errors'] = [ response['Error']['Message'] ]
+            return {
+                'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+                'body': json.dumps(body)
+            }
+
+        else:
+            logger.info('Set user password ({})'.format(new_user['email']))
+
+
+        # Create db item
+        logger.info('Creating user ({})'.format(new_user['email']))
         response = users_table.put_item(Item=new_user)
 
         if response['ResponseMetadata']['HTTPStatusCode'] != 200:
@@ -41,15 +93,14 @@ def lambda_handler(event, context):
             status_code = response['ResponseMetadata']['HTTPStatusCode']
             body['errors'] = [ response['Error']['Message'] ]
         else:
-            logger.info('Created user ({})'.format(new_user['username']))
+            logger.info('Created user ({})'.format(new_user['email']))
 
     except ClientError as e:
         logger.warn(e.response['Error']['Message'])
         status_code = e.response['ResponseMetadata']['HTTPStatusCode']
         body['errors'] = [ e.response['Error']['Message'] ]
-
-    # Create Cognito entry for this new user
-
+        
+ 
     return {
         'statusCode': status_code,
         'body': json.dumps(body)
